920760d501a31d35d375f8c9cac5d0cd472212c1
Zhaolin Lau <zhaolinlau@gmail.com>
2021-09-25 15:51:50 +0800

    Update user_profile.php

--- user_profile.php
+++ user_profile.php
@@ -9,13 +9,12 @@
     <link rel='stylesheet' href='index_font.css'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta charset="utf-8">
-    <!--  This file has been downloaded from bootdey.com @bootdey on twitter -->
-    <!--  All snippets are MIT license http://bootdey.com/license -->
-    <title>bs4 edit profile page - Bootdey.com</title>
+    <title>User Profile</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.1/dist/js/bootstrap.bundle.min.js"></script>
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
   <div class="w3-top">
@@ -47,7 +46,6 @@
    </div>
   </div>
   <br><br>
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
 
   <div class="col" style="float: right; width: 80%;">
     <div class="row">
