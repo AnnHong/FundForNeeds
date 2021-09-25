19a50c3bae3803a0391b9ca2b10ea12df64b0b68
Zhaolin Lau <zhaolinlau@gmail.com>
2021-09-26 07:47:04 +0800

    removed the untrustworthy bootstrap(detected by avast antivirus)

--- user_profile.php
+++ user_profile.php
@@ -13,7 +13,6 @@
 	<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.1/dist/js/bootstrap.bundle.min.js"></script>
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
   <div class="w3-top">
