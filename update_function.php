5d6280d032deeadffed7460d4e7c614d70b6df0e
AnnHong <khooannhong01@gmail.com>
2021-10-04 00:32:14 +0800

    modify coding

--- update_function.php
+++ update_function.php
@@ -2,9 +2,12 @@
 session_start();
 
 if(isset($_POST['ChangePassBtn'])){
+  UpdateUserPassword();
   //$email= $_SESSION['email'];
 //  $new_password =$_POST["new_password"];
-
+}
+function UpdateUserPassword(){
+  echo 'nak update : ';
   $con = mysqli_connect("localhost","fundforneeds","fundforneeds","fundforneeds");
 
 if(!$con){
@@ -12,14 +15,19 @@ if(!$con){
 }else{
   $sql = 'UPDATE users SET password="'.$_POST['newpassword'].'" WHERE email= "'.$_SESSION['email'].'"';
 
+  $qry=mysqli_query($con,$sql);
+  			return $qry;
+
   echo "<script>
           alert('Your password has been changed.');
           window.location.href='user_profile.php';
           </script>";;
 }
-
-
-
 }
+
+
+
+
+
 
  ?>
