b9b30c5fbd85fb9339825244ed63a8f12720336b
Zhaolin Lau <zhaolinlau@gmail.com>
2021-11-17 07:01:06 +0800

    add bulma css framework to patment succeed and payment failed pages

--- payment_succeed.php
+++ payment_succeed.php
@@ -4,6 +4,14 @@
     <meta charset="utf-8">
     <title>Payment Succeed</title>
     <script src="https://unpkg.com/vue@next"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@6.5.95/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <style media="screen">
+      div{
+        margin:18%;
+        text-align: center;
+      }
+    </style>
   </head>
   <body id="app">
     <payment-succeed></payment-succeed>
